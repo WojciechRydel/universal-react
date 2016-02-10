aa66bbcfa7f0ae687efc548480902fe2db71a095
RydelWojciech <rydel.wojciech@gmail.com>
2016-02-10 01:09:22 +0100

    files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	entry: [
+		'./browser.js'
+	],
+	output: {
+		path: './public',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [{test: /\.jsx?$/, loader: 'babel'}]
+	}
+}
